e378cf20a0d1f4b27d479a3ae6c2fd66e3fcf04c
ManoaRRR <hei.manoa.2@gmai.com>
2023-08-22 17:06:58 +0300

    feat: add EmployeeCnapsRepository

---
+++ src/main/java/com/example/prog4/repository/database2/EmployeeCnapsRepository.java
@@ -0,0 +1,11 @@
+package com.example.prog4.repository.database2;
+
+import com.example.prog4.repository.database2.entity.EmployeeCnaps;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeCnapsRepository extends JpaRepository<EmployeeCnaps, String> {
+    Optional<EmployeeCnaps> findByPersonalEmail(String personalEmail);
+
+}
---
+++ src/main/java/com/example/prog4/repository/database2/entity/EmployeeCnaps.java
@@ -0,0 +1,53 @@
+package com.example.prog4.repository.database2.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@Table(name = "\"employee_cnaps\"")
+public class EmployeeCnaps {
+    @Id
+    private String id;
+    private String cin;
+    private String cnaps;
+    private String image;
+    private String address;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "personal_email")
+    private String personalEmail;
+
+    @Column(name = "professional_email")
+    private String professionalEmail;
+
+    @Column(name = "registration_number")
+    private String registrationNumber;
+
+    private LocalDate birthdate;
+
+    @Column(name = "entrance_date")
+    private LocalDate entranceDate;
+
+    @Column(name = "departure_date")
+    private LocalDate departureDate;
+
+    @Column(name = "children_number")
+    private Integer childrenNumber;
+//    @Column(name = "end_to_end_id")
+//    private String EndToEndId;
+
+}
