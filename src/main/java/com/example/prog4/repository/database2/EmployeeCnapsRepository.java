7af8295a8cbe566b6bf5dc5288df373934a513b0
ManoaRRR <hei.manoa.2@gmai.com>
2023-08-25 09:32:00 +0300

    chore: add @Repository annotation

--- src/main/java/com/example/prog4/repository/database2/EmployeeCnapsRepository.java
+++ src/main/java/com/example/prog4/repository/database2/EmployeeCnapsRepository.java
@@ -2,8 +2,10 @@ package com.example.prog4.repository.database2;
 
 import com.example.prog4.repository.database2.entity.EmployeeCnaps;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+@Repository
 
 public interface EmployeeCnapsRepository extends JpaRepository<EmployeeCnaps, String> {
     Optional<EmployeeCnaps> findByPersonalEmail(String personalEmail);
